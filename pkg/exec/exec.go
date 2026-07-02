71fb758a8dc592cf0cb2c2a36a357e6f39e14a40
rumstead <37445536+rumstead@users.noreply.github.com>
2026-07-02 15:42:35 -0400

    feat: upgrade Golang version to 1.26 and update dependencies; enhance command handling and shell completion
    Signed-off-by: rumstead <37445536+rumstead@users.noreply.github.com>

--- pkg/exec/exec.go
+++ pkg/exec/exec.go
@@ -1,21 +1,8 @@
 package exec
 
 import (
-	"os"
 	"os/exec"
-	"syscall"
 )
-
-func KubeCtl(args []string) error {
-	path, err := exec.LookPath("kubectl")
-	if err != nil {
-		return err
-	}
-	// https://man7.org/linux/man-pages/man2/execve.2.html
-	// first arg in the argv list is the binary path
-	argv := append([]string{path}, args...)
-	return syscall.Exec(path, argv, os.Environ())
-}
 
 func ExecCmd(binary string, args ...string) (string, error) {
 	path, err := exec.LookPath(binary)
